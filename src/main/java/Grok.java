b368de76c6b55183c6b78c117f832064f72d514b
alexedwards <agedwards23@students.desu.edu>
2024-02-20 21:52:39 -0500

    hi

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -8,6 +8,78 @@
  * @version (0.1)
  */
 public class Grok
+
+
 {
+    private static int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+
+    /**
+     \* Initializes a Grok object to the default power level of 50.
+     */
+    public Grok() {
+        this(DEFAULT_POWER_LEVEL);
+    }
+
+
+
+
+
+
+
+    public Grok(int powerLevel){
+        this.powerLevel = powerLevel;
+
+
+        /**
+         *
+         * Initializes a Grok object to power powerLevel
+         */}
+    public int getPowerLevel(){
+
+
+        return powerLevel;
+    }
+    /*
+     * Returns the power level of this Grok.
+     * @return returns the power level of this Grok
+     */
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+     /*
+     * Sets the power level of this Grok.
+     * @param powerLevel the power value to set for this Grok.
+     */
+    public void takePowerPill(PowerPill pill){
+        ;
+        int pillPL = pill.getPower();
+        powerLevel += pillPL;
+
+
+
+
+
+    }
+    /*
+            * Set the power level of this Grok to the power level of
+  * the pill.
+        * @param pill The PowerPill that the this Grok.  The power
+  * of the pill is added to the power level of this Grok.
+  */
     // implement this class per instructions in Required Activities.
+
+    public void tookHit(){
+        powerLevel -= 5;
+
+
+
+
+
+    }
+
+
+
+
 }
